47608067c0278e67d9188e5aa7e7ffe5a524f9c9
Daniella J-Lany <ddzheylani@gmail.com>
2025-11-29 12:22:23 +0300

    chore(5) Create auth flow

--- src/features/auth/signin-form.tsx
+++ src/features/auth/signin-form.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Card } from '@/components/ui/card';
-import FormContent from '@/features/auth/form-content';
+import LoginFormContent from '@/features/auth/login-form-content';
 
-export function SignupForm(props: React.ComponentProps<typeof Card>) {
+export function SigninForm(props: React.ComponentProps<typeof Card>) {
   return (
     <div className="w-full max-w-sm">
       <Card
         {...props}
         className="hidden md:block border-none rounded-2xl inset-shadow-2xs inset-shadow-sidebar-accent bg-white/95 backdrop-blur p-8"
       >
-        <FormContent />
+        <LoginFormContent />
       </Card>
       <div className="md:hidden">
-        <FormContent />
+        <LoginFormContent />
       </div>
     </div>
   );
