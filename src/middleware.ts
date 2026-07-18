d771b84f271c777b65c4ebe84187468a95347413
J-Lany <ddzheylani@gmail.com>
2026-07-18 16:31:32 +0300

    feat(middleware): update matcher to exclude icon.webp from middleware routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|api|static|favicon.ico).*)'],
+  matcher: ['/((?!_next|api|static|favicon.ico|icon.webp).*)'],
 };
